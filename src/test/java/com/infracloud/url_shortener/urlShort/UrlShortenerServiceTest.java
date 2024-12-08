1400d10e26e5d3a458002c8b3e8e1c61e06c0086
Devendra274 <devendrataleleg2016@gmail.com>
2024-12-08 13:49:22 +0530

    Controller and Service test case for redirect and metrics endpoint

--- src/test/java/com/infracloud/url_shortener/urlShort/UrlShortenerServiceTest.java
+++ src/test/java/com/infracloud/url_shortener/urlShort/UrlShortenerServiceTest.java
@@ -1,5 +1,6 @@
 package com.infracloud.url_shortener.urlShort;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -16,12 +17,14 @@ class UrlShortenerServiceTest {
     @Autowired
     UrlShortenerService urlShortenerService;
 
+    private static final String BASE_URL = "http://localhost/";
+
     @ParameterizedTest
     @MethodSource
     void test_createShortUrl(String originalUrl) {
         //Given
         int port = 8090;
-        String baseUrl="http://localhost";
+        String baseUrl="http://localhost/";
 
         //When
         String shortUrl = urlShortenerService.createShortUrl(originalUrl, baseUrl);
@@ -35,4 +38,45 @@ class UrlShortenerServiceTest {
                 Arguments.of("https://example.com"),
                 Arguments.of("https://example.com/order")
         );
-    }}
+    }
+
+    @Test
+    void testRedirectURL() {
+        //Given
+        String originalUrl = "https://example.com";
+
+        //When
+        String shortUrl = urlShortenerService.createShortUrl(originalUrl, BASE_URL);
+        shortUrl = shortUrl.split("/")[3];
+        System.out.println(shortUrl);
+
+        String redirectedUrl = urlShortenerService.redirectURL(shortUrl);
+        System.out.println(redirectedUrl);
+
+        //Then
+        assertEquals(originalUrl, redirectedUrl);
+    }
+
+    @Test
+    void testMetrics() {
+        //Given
+        urlShortenerService.createShortUrl("https://youtube.com/account", BASE_URL);
+        urlShortenerService.createShortUrl("https://youtube.com/admin", BASE_URL);
+        urlShortenerService.createShortUrl("https://youtube.com/audio", BASE_URL);
+        urlShortenerService.createShortUrl("https://udemy.com/user", BASE_URL);
+        urlShortenerService.createShortUrl("https://udemy.com/trainer", BASE_URL);
+        urlShortenerService.createShortUrl("https://udemy.com/course", BASE_URL);
+        urlShortenerService.createShortUrl("https://irctc.com/trains", BASE_URL);
+        urlShortenerService.createShortUrl("https://irctc.com/agent", BASE_URL);
+        urlShortenerService.createShortUrl("https://asics.com/shoes", BASE_URL);
+
+        //When
+        var metrics = urlShortenerService.getTopDomains();
+
+        //Then
+        assertEquals(3, metrics.get("youtube.com"));
+        assertEquals(3, metrics.get("udemy.com"));
+        assertEquals(2, metrics.get("irctc.com"));
+        assertNull(metrics.get("asics.com"));
+    }
+}
